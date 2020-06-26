404858cbf719a3ffa98bc9afec7d5fcab7134fef
Dalton Smith <darasmith@wccnet.edu>
2020-06-26 12:07:18 -0400

    Update submodule (#3)
    * Update submodule
    
    * Update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,6 +10,9 @@ MAINPROJECT=frc-docs
 # Set working directory to repo root
 cd `dirname $0`/..
 
+# Update submodule
+git submodule update --recursive
+
 # Create POT Files
 sphinx-build -T -b gettext $MAINPROJECT/source locale/pot
 

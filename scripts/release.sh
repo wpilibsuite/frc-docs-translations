b35a0ebeb015ac338bbbd1b7a53aae7793219282
Austin Shalit <austinshalit@gmail.com>
2020-06-26 00:30:03 -0700

    Store POT files in repo; pull and push translations

--- scripts/release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cp frc-docs/.tx/config .tx/config
-tx pull -l fr_CA,es_MX --mode onlyreviewed --use-git-timestamps
---
+++ scripts/update.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# update transifex pot and local po files
+
+set -ex
+
+LANG_TO_PULL=${1:-'fr_CA,es_MX'}
+LANG_MAP='es_MX: es, fr_CA: fr, he_IL: he, tr_TR: tr'
+MAINPROJECT=frc-docs
+
+# Set working directory to repo root
+cd `dirname $0`/..
+
+# Create POT Files
+sphinx-build -T -b gettext $MAINPROJECT/source locale/pot
+
+# Update .tx/config
+mv .tx/config .tx/config.old
+sphinx-intl create-txconfig
+echo "lang_map = ${LANG_MAP}" >> .tx/config
+sphinx-intl update-txconfig-resources -p locale/pot -d locale --transifex-project-name $MAINPROJECT
+# TODO: Compare new and old configs and lock or remove deleted files on transifex
+rm .tx/config.old
+
+# Push and pull from Transifex. It is important to push then pull!
+# If you pull then push, the PO files will contain out of date strings.
+if [ "$CI" = true ]
+then
+    echo "Here is where we would push"
+    # Uncomment when we are ready to make transifex changes
+    # tx push --source --no-interactive --skip
+fi
+tx pull -l $LANG_TO_PULL --mode onlyreviewed --use-git-timestamps

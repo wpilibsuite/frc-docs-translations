8508d851c86e7c5808f3715aba10f3a38c8ec9bb
Daltz333 <daltzsmith@gmail.com>
2022-02-21 17:08:36 -0500

    Implement Transifex APIv3

--- conf.py
+++ conf.py
@@ -12,15 +12,16 @@ from sphinx.util.pycompat import execfile_
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
-execfile_(os.path.join(BASEDIR, 'frc-docs/source/conf.py'), globals())
+execfile_(os.path.join(BASEDIR, "frc-docs/source/conf.py"), globals())
 
-locale_dirs = [os.path.join(BASEDIR, 'locale/')]
+locale_dirs = [os.path.join(BASEDIR, "locale/")]
 gettext_compact = False
 
-setup_original = setup 
+setup_original = setup
+
 
 def setup(app):
-    app.srcdir = os.path.join(BASEDIR, 'frc-docs/source/')
+    app.srcdir = os.path.join(BASEDIR, "frc-docs/source/")
     app.confdir = app.srcdir
 
     setup_original(app)
--- scripts/lock-translations.py
+++ scripts/lock-translations.py
@@ -1,36 +1,70 @@
 import requests
 import os
 import argparse
+import json
 import configparser
 import dateutil.parser
 import dateutil.utils
+import dateutil
 
-TX_TOKEN = os.getenv('TX_TOKEN')
-RESOURCES_URL = 'https://www.transifex.com/api/2/project/frc-docs/resources/'
-RESOURCE_BASE_URL = 'https://www.transifex.com/api/2/project/frc-docs/resource/'
+TX_TOKEN = os.getenv("TX_TOKEN")
+RESOURCES_URL = "https://rest.api.transifex.com/resources"
+PROJECT_FILTER_PARAM = {"filter[project]": "o:wpilib:p:frc-docs"}
+PROJECT_DELETE_PARAM = {"o:wpilib:p:frc-docs:r:"}  # resource slug appended to end
 
-lock_json = """
-    {
-        "accept_translations":"False"
+lock_json = {
+    "data": {
+        "attributes": {"accept_translations": False},
+        "id": "",
+        "type": "resources",
     }
-"""
+}
 
-headers = {"Content-type": "application/json"}
+headers = {
+    "Authorization": f"Bearer {TX_TOKEN}",
+    "Content-Type": "application/vnd.api+json",
+}
 
-details_param = {"details":""}
+details_param = {"details": ""}
+
 
 def get_remote_resources():
-    remote_request = requests.get(RESOURCES_URL, auth=('api', TX_TOKEN))
+    remote_request = requests.get(
+        RESOURCES_URL, headers=headers, params=PROJECT_FILTER_PARAM
+    )
 
     if remote_request.status_code != 200:
         print("Error retrieving remote resources", remote_request.status_code)
         exit(1)
     else:
-        print("Successfully retrieved remote resources!")
+        print("Successfully retrieved remote resources page 1!")
 
-    resources = []
-    for resource in remote_request.json():
-        resources.append(resource['slug'])
+    data = remote_request.json()
+
+    resources = {}
+    for resource in data["data"]:
+        resources[resource["attributes"]["slug"]] = resource
+
+    pagination = 1
+    while True:  # Exit if no more pages to paginate
+        if data["links"]["next"] is None:
+            break
+        else:
+            next_page = data["links"]["next"].split("page[cursor]=")[1]
+            PROJECT_FILTER_PARAM.update({"page[cursor]": next_page})  # Begin pagination
+            remote_request = requests.get(
+                RESOURCES_URL, headers=headers, params=PROJECT_FILTER_PARAM
+            )
+
+            if remote_request.status_code != 200:
+                print("Error retrieving remote resources", remote_request.status_code)
+                exit(1)
+            else:
+                pagination += 1
+                print(f"Successfully retrieved remote resources page {pagination}!")
+                data = remote_request.json()
+                for resource in data["data"]:
+                    resources[resource["attributes"]["slug"]] = resource
 
     return resources
 
@@ -38,13 +72,13 @@ def get_remote_resources():
 def get_local_resources(tx_config):
     config = configparser.ConfigParser()
     config.read(tx_config)
-    
+
     resources = []
     for section in config.sections():
         section = section.split("frc-docs.", 1)
-        
+
         # Eliminate the main configuration, and remote project identifier
-        if section[0] == 'main':
+        if section[0] == "main":
             continue
         else:
             resources.append(section[1])
@@ -55,56 +89,46 @@ def get_local_resources(tx_config):
 
 
 def get_unused_resources(remote_resources, local_resources):
-    unused_resources = []
-    for resource in remote_resources:
+    unused_resources = {}
+    for resource in remote_resources.keys():
         if resource not in local_resources:
-            response = requests.get(RESOURCE_BASE_URL + resource + "/",
-                    params=details_param,
-                    auth=("api", TX_TOKEN))
+            resource_data = remote_resources[resource]
+            if not resource_data["attributes"]["accept_translations"]:
+                last_update = dateutil.parser.parse(
+                    resource_data["attributes"]["datetime_modified"]
+                )
+                current_time = dateutil.utils.today().replace(tzinfo=dateutil.tz.UTC)
 
-            if response.status_code != 200:
-                print("Error retrieving information for resource:", resource)
-                # prevent script from hanging on error
-                continue
-            elif response.status_code == 404:
-                print("Resource not found!", resource)
-                # deleted, no need to worry
-                continue
-
-            json_response = response.json()
-            
-            if not json_response["accept_translations"]:
-                last_update = dateutil.parser.parse(json_response["last_update"])
-                current_time = dateutil.utils.today()
-                
                 delete_status = (current_time - last_update).days >= 3
-
                 if delete_status:
-                    response = requests.delete(RESOURCE_BASE_URL + resource + "/",
-                            auth=("api", TX_TOKEN))
+                    TEMP_RESOURCES_URL = RESOURCES_URL + "/" + resource_data["id"]
+                    response = requests.delete(TEMP_RESOURCES_URL, headers=headers)
 
                     if response.status_code != 204:
-                        print("Error deleting resource ", resource, response.status_code)
+                        print(
+                            "Error deleting resource ", resource, response.status_code
+                        )
                     else:
                         print("Successfully deleted old resource", resource)
 
                 continue
 
-            unused_resources.append(resource)
+            unused_resources[resource] = resource_data
 
     return unused_resources
 
 
 def lock_resources(unused_resources):
     err = False
-    for resource in unused_resources:
-        response = requests.put(RESOURCE_BASE_URL + resource + "/",
-                    auth=("api", TX_TOKEN),
-                    headers=headers,
-                    data=lock_json)
+    for resource in unused_resources.keys():
+        TEMP_RESOURCES_URL = RESOURCES_URL + "/" + unused_resources[resource]["id"]
+        lock_json["data"]["id"] = unused_resources[resource]["id"]
+        lock = json.dumps(lock_json)
+        response = requests.patch(TEMP_RESOURCES_URL, headers=headers, data=lock)
 
         if response.status_code != 200:
             print("Error locking resource:", response, resource)
+            print(response.content)
             err = True
         else:
             print("Successfully locked resource:", resource)
@@ -112,16 +136,18 @@ def lock_resources(unused_resources):
     if err:
         print("Script exited with problems!")
         exit(1)
-        
+
 
 def main():
-    arg_parser = argparse.ArgumentParser(description="locks the unused files on Transifex")
+    arg_parser = argparse.ArgumentParser(
+        description="locks the unused files on Transifex"
+    )
     arg_parser.add_argument("tx_config_path", type=str, help="path to Transifex config")
 
     args = vars(arg_parser.parse_args())
 
     tx_config_path = args["tx_config_path"]
-    
+
     print("Using TX Config Path:", tx_config_path)
 
     remote_resources = get_remote_resources()
@@ -141,5 +167,5 @@ def main():
             print("Skipping lock because not running on CI")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

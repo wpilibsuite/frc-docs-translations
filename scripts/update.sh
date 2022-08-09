0d83e3b2176287cb86710738b00b5364dca34624
Dalton Smith <daltzsmith@gmail.com>
2022-08-08 21:30:03 -0400

    Update transifex api for APIv3 changes (#32)
    * Update to new transifex
    
    * Finialize updates for Transifex APIv3
    
    * Fix submodule version
    
    * Fix more barf

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,6 +6,7 @@ set -ex
 LANG_TO_PULL=${1:-'fr_CA,es_MX,zh_CN,tr_TR,he_IL,pt'}
 LANG_MAP='es_MX: es, fr_CA: fr, he_IL: he, tr_TR: tr'
 MAINPROJECT=frc-docs
+ORGANIZATION=wpilib
 
 # Set working directory to repo root
 cd `dirname $0`/..
@@ -17,12 +18,12 @@ sphinx-build -T -b gettext $MAINPROJECT/source locale/pot
 rm .tx/config
 sphinx-intl create-txconfig
 echo "lang_map = ${LANG_MAP}" >> .tx/config
-sphinx-intl update-txconfig-resources -p locale/pot -d locale --transifex-project-name $MAINPROJECT
+sphinx-intl update-txconfig-resources -p locale/pot -d locale --transifex-project-name $MAINPROJECT --transifex-organization-name $ORGANIZATION
 
 # Push and pull from Transifex. It is important to push then pull!
 # If you pull then push, the PO files will contain out of date strings.
 if [ "$CI" = true ]
 then
-    tx push --source --no-interactive --skip
+    tx push --source --skip
 fi
 tx pull -l $LANG_TO_PULL --mode onlyreviewed --use-git-timestamps
